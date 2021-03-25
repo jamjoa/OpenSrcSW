eda6dc45af09e9cdf3a5c856017408cfe2f81a2b
jamjoa <kkujoaa@gmail.com>
2021-03-25 16:26:21 +0900

    계산 오류 수정, 소수점 셋째 자리에서 반올림으로 수정

--- practice01/src/konkuk/jamjoa/indexer.java
+++ practice01/src/konkuk/jamjoa/indexer.java
@@ -12,6 +12,7 @@ import java.io.*;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.List;
 
 public class indexer {
 
@@ -22,13 +23,11 @@ public class indexer {
 
         System.out.println("일어온 객체의 type -> "+object.getClass());
 
-        HashMap<String, ArrayList<Double>> wgtMap = (HashMap<String, ArrayList<Double>>)object;
-        Iterator<String> it=wgtMap.keySet().iterator();
+        HashMap<String, List<String>> wgtMap = (HashMap<String, List<String>>)object;
 
-        while(it.hasNext()){
-            String key=it.next();
-            System.out.print(key+" -> ");
-            ArrayList<Double> values=wgtMap.get(key);
+        for (String key : wgtMap.keySet()) {
+            System.out.print(key + " -> ");
+            List<String> values = wgtMap.get(key);
             System.out.println(values);
         }
 
@@ -45,14 +44,14 @@ public class indexer {
         NodeList docList = inputdoc.getElementsByTagName("doc");
 
         //전체 문서에서 몇번 나오는지 세기 위한 hashmap
-        HashMap<String, Integer> DocMap = new HashMap<String, Integer>();
+        HashMap<String, Integer> DocMap = new HashMap<>();
         int N = docList.getLength();
         for (int index = 0; index < N; index++) {
             Element curDoc = (Element) docList.item(index);
             //바디 태그 내용 가져옴
             String docBody = curDoc.getElementsByTagName("body").item(0).getTextContent();
             //:와 #으로 구분하므로 0번,2번,4번,... 에 단어가 저장되어 있음
-            String[] slices = docBody.split(":|#");//떡,16,멥쌀,4,...
+            String[] slices = docBody.split("[:#]");//떡,16,멥쌀,4,...
             for (int i = 0; i < slices.length; i += 2) {
                 int value = 1;
                 if (DocMap.containsKey(slices[i])) {
@@ -62,26 +61,26 @@ public class indexer {
             }
         }
         //전체 문서에서 각 단어 몇 번 나오는지 셌으니 (dfx) 가중치 계산
-        HashMap<String, ArrayList<Double>> wgtMap = new HashMap<String, ArrayList<Double>>();
-        for (int index = 0; index < docList.getLength(); index++) {
+        HashMap<String, List<String>> wgtMap = new HashMap<>();
+        for (int index = 0; index < N; index++) {
             Element curDoc = (Element) docList.item(index);
             //바디 태그 내용 가져옴
             String docBody = curDoc.getElementsByTagName("body").item(0).getTextContent();
             //:와 #으로 구분하므로 0번,2번,4번,... 에 단어가 저장되어 있음
-            String[] slices = docBody.split(":|#");
+            String[] slices = docBody.split("[:#]");
             for (int i = 0; i < slices.length; i += 2) {
-
-                int count = Integer.parseInt(slices[i + 1]);
-                double w = count * Math.log(N / (double)count);
-                ArrayList<Double> wList =wgtMap.get(slices[i]);
+                int count = DocMap.get(slices[i]);
+                double w = Integer.parseInt(slices[i+1]) * Math.log(N / (double)count);
+                w=Math.round(w*100)/100.0;
+                List<String> wList =wgtMap.get(slices[i]);
                 if (wList == null) {
-                    wList = new ArrayList<Double>();
-                    wList.add((double) index);
-                    wList.add((Math.round(w*10)/10.0));
+                    wList = new ArrayList<>();
+                    wList.add(Integer.toString(index));
+                    wList.add(Double.toString(w));
                     wgtMap.put(slices[i],wList);
                 }else{
-                    wList.add((double) index);
-                    wList.add((Math.round(w*10)/10.0));
+                    wList.add(Integer.toString(index));
+                    wList.add(Double.toString(w));
                 }
 
             }
