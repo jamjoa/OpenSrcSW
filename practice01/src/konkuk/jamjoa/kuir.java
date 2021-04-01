3fb1c836d66e0821da710c574f6d4d1a4e8e5fdb
jamjoa <kkujoaa@gmail.com>
2021-04-01 14:11:34 +0900

    Calculate similarity, make new branch which name is feature

--- practice01/src/konkuk/jamjoa/kuir.java
+++ practice01/src/konkuk/jamjoa/kuir.java
@@ -1,7 +1,9 @@
 package konkuk.jamjoa;
 
 
+import org.w3c.dom.*;
 import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
 import javax.xml.parsers.DocumentBuilder;
@@ -9,6 +11,9 @@ import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.TransformerException;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.Collections;
+
 
 public class kuir {
 
@@ -23,6 +28,39 @@ public class kuir {
             indexer indexer=new indexer(args[1]);
             //index.post 파일 확인
             //indexer.show();
+        }if(args[0].equals("-s")){
+            searcher searcher=new searcher(args[1]);
+
+            if(args[2].equals("-q")){
+                Double[] similarities = searcher.calcSimilarity(args[3]);
+                boolean[] check=new boolean[similarities.length];
+//                for(int i=0;i<similarities.length;i++){
+//                    System.out.println(similarities[i]);
+//                }
+                Double[] sorted= Arrays.copyOf(similarities, similarities.length);
+                Arrays.sort(sorted, Collections.reverseOrder());
+
+//                for(int i=0;i<3;i++){
+//                    System.out.println(sorted[i]);
+//                }
+
+                String collectionDir=args[1].replace("index.post","collection.xml");//문서 몇개인지
+                DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+                DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
+                Document inputDoc = docBuilder.parse(collectionDir);
+                NodeList docList = inputDoc.getElementsByTagName("doc");
+
+                for(int j=0;j<3;j++){
+                    for(int i=0;i<similarities.length;i++) {
+                        if (similarities[i].equals(sorted[j]) && !check[i]) {
+                            String tText = ((Element) docList.item(i)).getElementsByTagName("title").item(0).getTextContent();
+                            System.out.println(tText);
+                            check[i] = true;
+                        }
+                    }
+                }
+
+            }
         }
 
     }
---
+++ practice01/src/konkuk/jamjoa/searcher.java
@@ -0,0 +1,74 @@
+package konkuk.jamjoa;
+
+import org.snu.ids.kkma.index.Keyword;
+import org.snu.ids.kkma.index.KeywordExtractor;
+import org.snu.ids.kkma.index.KeywordList;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.util.HashMap;
+import java.util.List;
+
+public class searcher {
+    String dir;
+    public searcher(String dir) {
+        this.dir=dir;
+    }
+
+    public Double[] calcSimilarity(String query) throws ParserConfigurationException, IOException, SAXException, ClassNotFoundException {
+
+        KeywordExtractor ke = new KeywordExtractor();
+        KeywordList kl = ke.extractKeyword(query, true);
+
+        String[][] keywords=new String[kl.size()][2];
+
+        int count=0;
+        for (Keyword k : kl){
+            keywords[count][0]=k.getString();
+            keywords[count][1]=Integer.toString(1);
+            count++;
+        }
+
+        String collectionDir=dir.replace("index.post","collection.xml");//문서 몇개인지
+        DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
+        Document inputdoc = docBuilder.parse(collectionDir);
+        NodeList docList = inputdoc.getElementsByTagName("doc");
+
+        double[][] weights=new double[keywords.length][docList.getLength()]; //모두 0
+
+        FileInputStream fileStream = new FileInputStream(dir);
+        ObjectInputStream objectInputStream=new ObjectInputStream(fileStream);
+        Object object = objectInputStream.readObject();
+        HashMap<String, List<String>> wgtMap = (HashMap<String, List<String>>)object;
+
+        for(int i=0;i< keywords.length;i++){
+            List<String> values=wgtMap.get(keywords[i][0]);
+            for(int j=0;j<values.size();j+=2){
+                weights[i][Integer.parseInt(values.get(j))]=Double.parseDouble(values.get(j+1));
+            }
+        }
+        objectInputStream.close();
+
+        Double[] similarities=new Double[docList.getLength()];
+
+        for(int i=0;i< docList.getLength();i++){
+            double similarity=0;
+            for(int j=0;j<keywords.length;j++){
+                similarity+=Integer.parseInt(keywords[j][1])*weights[j][i];
+            }
+            similarity=Math.round(similarity*100)/100.0;
+            similarities[i]=similarity;
+
+        }
+        return similarities;
+
+    }
+}
