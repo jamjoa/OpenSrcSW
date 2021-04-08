a625d2f2e6b73ec9ef7bee49b0f6470421571bbf
jamjoa <kkujoaa@gmail.com>
2021-04-08 15:00:13 +0900

    Calculate cosine similarites by using inner product

--- practice01/src/konkuk/jamjoa/kuir.java
+++ practice01/src/konkuk/jamjoa/kuir.java
@@ -17,49 +17,61 @@ import java.util.Collections;
 
 public class kuir {
 
+    public static void showTopDoc(Double[] similarities, String dir) throws ParserConfigurationException, IOException, SAXException {
+        boolean[] check = new boolean[similarities.length];
+//        System.out.println("========정렬전=========");
+//        for (int i = 0; i < similarities.length; i++) {
+//            System.out.println(similarities[i]);
+//        }
+//        System.out.println("======================");
+        Double[] sorted = Arrays.copyOf(similarities, similarities.length);
+        Arrays.sort(sorted, Collections.reverseOrder());
+//        System.out.println("========정렬후=========");
+//        for (int i = 0; i < 3; i++) {
+//            System.out.println(sorted[i]);
+//        }
+//        System.out.println("======================");
+        String collectionDir = dir.replace("index.post", "collection.xml");//문서 몇개인지
+        DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
+        Document inputDoc = docBuilder.parse(collectionDir);
+        NodeList docList = inputDoc.getElementsByTagName("doc");
+
+        for (int j = 0; j < 3; j++) {
+            if (sorted[j] == 0) {
+                if (j == 0) System.out.println("문서 결과가 없습니다.");
+                break;
+            }
+            for (int i = 0; i < similarities.length; i++) {
+                if (similarities[i].equals(sorted[j]) && !check[i]) {
+                    String tText = ((Element) docList.item(i)).getElementsByTagName("title").item(0).getTextContent();
+                    System.out.println(tText);
+                    check[i] = true;
+                    break;
+                }
+            }
+        }
+    }
+
     public static void main(String[] args) throws ParserConfigurationException, TransformerException, SAXException, IOException, ClassNotFoundException {
 
-        if(args[0].equals("-c")){
+        if (args[0].equals("-c")) {
             makeCollection makeCollection = new makeCollection(args[1]);
         }
-        if(args[0].equals("-k")){
-            makeKeyword makeKeyword=new makeKeyword(args[1]);
-        }if(args[0].equals("-i")){
-            indexer indexer=new indexer(args[1]);
+        if (args[0].equals("-k")) {
+            makeKeyword makeKeyword = new makeKeyword(args[1]);
+        }
+        if (args[0].equals("-i")) {
+            indexer indexer = new indexer(args[1]);
             //index.post 파일 확인
             //indexer.show();
-        }if(args[0].equals("-s")){
-            searcher searcher=new searcher(args[1]);
+        }
+        if (args[0].equals("-s")) {
+            searcher searcher = new searcher(args[1]);
 
-            if(args[2].equals("-q")){
+            if (args[2].equals("-q")) {
                 Double[] similarities = searcher.calcSimilarity(args[3]);
-                boolean[] check=new boolean[similarities.length];
-//                for(int i=0;i<similarities.length;i++){
-//                    System.out.println(similarities[i]);
-//                }
-                Double[] sorted= Arrays.copyOf(similarities, similarities.length);
-                Arrays.sort(sorted, Collections.reverseOrder());
-
-//                for(int i=0;i<3;i++){
-//                    System.out.println(sorted[i]);
-//                }
-
-                String collectionDir=args[1].replace("index.post","collection.xml");//문서 몇개인지
-                DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
-                DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
-                Document inputDoc = docBuilder.parse(collectionDir);
-                NodeList docList = inputDoc.getElementsByTagName("doc");
-
-                for(int j=0;j<3;j++){
-                    for(int i=0;i<similarities.length;i++) {
-                        if (similarities[i].equals(sorted[j]) && !check[i]) {
-                            String tText = ((Element) docList.item(i)).getElementsByTagName("title").item(0).getTextContent();
-                            System.out.println(tText);
-                            check[i] = true;
-                        }
-                    }
-                }
-
+                showTopDoc(similarities, args[1]);
             }
         }
 
--- practice01/src/konkuk/jamjoa/searcher.java
+++ practice01/src/konkuk/jamjoa/searcher.java
@@ -18,16 +18,12 @@ import java.util.List;
 
 public class searcher {
     String dir;
+    int docCnt=0;
     public searcher(String dir) {
         this.dir=dir;
     }
-    
-    public Double[] calcSimilarity2(String query) {
-        return null;
-    }
 
-    public Double[] InnerProduct(String query) throws ParserConfigurationException, IOException, SAXException, ClassNotFoundException {
-
+    public String[][] getKeywordsList(String query){
         KeywordExtractor ke = new KeywordExtractor();
         KeywordList kl = ke.extractKeyword(query, true);
 
@@ -39,13 +35,16 @@ public class searcher {
             keywords[count][1]=Integer.toString(1);
             count++;
         }
+        return keywords;
+    }
 
+    public double[][] getWeight(String[][] keywords) throws ParserConfigurationException, IOException, SAXException, ClassNotFoundException{
         String collectionDir=dir.replace("index.post","collection.xml");//문서 몇개인지
         DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
         DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
         Document inputdoc = docBuilder.parse(collectionDir);
         NodeList docList = inputdoc.getElementsByTagName("doc");
-
+        this.docCnt=docList.getLength();
         double[][] weights=new double[keywords.length][docList.getLength()]; //모두 0
 
         FileInputStream fileStream = new FileInputStream(dir);
@@ -60,18 +59,78 @@ public class searcher {
             }
         }
         objectInputStream.close();
+        return weights;
+    }
 
-        Double[] similarities=new Double[docList.getLength()];
+    public void printWeight(double[][] weights){
+        System.out.println("===============================");
+        System.out.print(" "+"\t");
+        for(int i=0;i<weights[0].length;i++){
+            System.out.print(i+"\t");
+        }
+        System.out.println();
+        for(int i=0;i< weights.length;i++){
+            System.out.print(i+"\t");
+            for(int j=0;j< weights[i].length;j++){
+                System.out.print(weights[i][j]+"\t");
+            }
+            System.out.println();
+        }
+        System.out.println("===============================");
+    }
 
-        for(int i=0;i< docList.getLength();i++){
+
+    public Double[] calcSimilarity(String query) throws ClassNotFoundException, ParserConfigurationException, SAXException, IOException {
+
+        Double[] similarities=this.InnerProduct(query);
+        String[][] keywords=this.getKeywordsList(query);
+        double[][] weights=this.getWeight(keywords);
+        //printWeight(weights);
+
+        double queryMag=0;//쿼리의 크기
+        for(int i=0;i<keywords.length;i++){
+            int w=Integer.parseInt(keywords[i][1]);
+            queryMag+=w*w;
+        }
+        queryMag=Math.sqrt(queryMag);
+
+        for(int i=0;i<docCnt;i++){
+            double docMag=0;
+            for(int j=0;j< keywords.length;j++){
+                double w=weights[j][i];
+                docMag+=w*w;
+            }
+            docMag=Math.sqrt(docMag);
+            if(similarities[i]!=0) {
+                similarities[i] = similarities[i] / (queryMag * docMag);
+                similarities[i]=Math.round(similarities[i]*100)/100.0;
+            }
+        }
+//        for(double d : similarities){
+//            System.out.println(d);
+//        }
+        return similarities;
+    }
+
+    public Double[] InnerProduct(String query) throws ClassNotFoundException, ParserConfigurationException, SAXException, IOException {
+
+        String[][] keywords=this.getKeywordsList(query);
+        double[][] weights=this.getWeight(keywords);
+
+        Double[] similarities=new Double[docCnt];
+        for(int i=0;i< docCnt;i++){
             double similarity=0;
             for(int j=0;j<keywords.length;j++){
                 similarity+=Integer.parseInt(keywords[j][1])*weights[j][i];
             }
             similarity=Math.round(similarity*100)/100.0;
             similarities[i]=similarity;
-
         }
+//        System.out.println("=====inner Product 결과=====");
+//        for(double d : similarities){
+//            System.out.println(d);
+//        }
+//        System.out.println("===========================");
         return similarities;
 
     }
